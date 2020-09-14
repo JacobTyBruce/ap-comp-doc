8e2c4745aa0714f87096461d5e62bad65488ba58
JacobTyBruce <jacobtybruce@gmail.com>
2020-09-14 18:21:48 +0000

    Started connection to MongoDB, need to fix

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,24 @@
 const express = require('express');
-const history = require('connect-history-api-fallback');
+/*const history = require('connect-history-api-fallback');
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs');*/
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://jake:<JKee2k20>@cluster0.gczfd.mongodb.net/<sample_restaraunts>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("smaple_restaraunts").collection("restaraunts");
+  // perform actions on the collection object
+  console.log(collection);
+  client.close();
+});
+
 
 const app = express();
-const staticFileMiddleware = express.static('../dist');
-app.use(staticFileMiddleware);
-app.use(history({
-    index: '../dist/index.html'
-}));
-app.use(staticFileMiddleware);
 
 app.get('/', (req, res) => {
     res.sendFile('../dist/index.html')
 })
 
-app.listen(8080, '127.0.0.1', () => {
+app.listen(8080, () => {
     console.log('Listening on port 8080');
 });
