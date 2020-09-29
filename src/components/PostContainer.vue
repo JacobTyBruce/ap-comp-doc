4d7b8e244bb11fd2135b27c605d967481b9c314b
JacobTyBruce <jacobtybruce@gmail.com>
2020-09-28 22:39:06 -0400

    fixed Delete Button

--- src/components/PostContainer.vue
+++ src/components/PostContainer.vue
@@ -1,18 +1,20 @@
 <template>
-  <v-container class='text-center'>
-  <v-row class='mb-4' justify="center">
-      <v-btn @click="$router.back()" class='ml-4' color='orange'> Back </v-btn>
+  <v-container class="text-center">
+    <v-row class="mb-4" justify="center">
+      <v-btn @click="$router.back()" class="ml-4" color="orange">Back</v-btn>
     </v-row>
     <v-row justify="center">
-    <v-card>
-      <v-card-title>{{this.postContent.title}}</v-card-title>
-      <v-card-subtitle v-html="this.postContent.text"></v-card-subtitle>
-    <v-divider></v-divider>
-      <v-card-text>Posted By {{this.postContent.postedBy}} at {{new Date(postContent.createdAt).toLocaleString('en-US')}}</v-card-text>
-    </v-card>
+      <v-card>
+        <v-card-title>{{this.postContent.title}}</v-card-title>
+        <v-card-subtitle v-html="this.postContent.text"></v-card-subtitle>
+        <v-divider></v-divider>
+        <v-card-text>Posted By {{this.postContent.postedBy}} at {{new Date(postContent.createdAt).toLocaleString('en-US')}}</v-card-text>
+      </v-card>
     </v-row>
     <br />
-    <v-row v-id='isAdmin == true' justify='center'><v-btn @click='deletePost' color='red'>Delete</v-btn></v-row>
+    <v-row v-if="isAdmin == true" justify="center">
+      <v-btn @click="deletePost" color="red">Delete</v-btn>
+    </v-row>
   </v-container>
 </template>
 
@@ -22,35 +24,46 @@ export default {
   data: function () {
     return {
       postContent: this.$store.state.currentDataSet,
-      isAdmin: this.isAdminComp
+      isAdmin: false,
     };
   },
   methods: {
-      deletePost() {
-          alert(this.postContent)
-          this.$http.delete(`${process.env.VUE_APP_API_URL}/api/delete/posts`, this.postContent).then(result => {
-              alert(result)
-          })
-      }
+    deletePost() {
+      alert(this.postContent);
+      console.log(this.postContent);
+      this.$http
+        .delete(
+          `${process.env.VUE_APP_API_URL}/api/delete/posts`,
+          this.postContent
+        )
+        .then((result) => {
+          alert(result);
+          console.log(result);
+        });
+    },
   },
   beforeCreate() {
-      // route back if first nav -- help with state
+    // route back if first nav -- help with state
     if (this.$store.state.currentDataSet.hasOwnProperty("title") == false) {
       this.$router.push("/community/posts");
     }
-    
-  },
-  computed: {
-      isAdminComp() {
-          var account = store.state.userAccount;
-            this.$http.get(`${process.env.VUE_APP_API_URL}/api/get/users/?userId=${account.userId}`).then(result => {
-                if (result.data[0].roles.includes('admin')) {
-                    return true;
-                } else {
-                    return false;
-                }
-            })
-        }
-    }
+
+    // delete button
+    var account = this.$store.state.userAccount;
+      console.log(account);
+      this.$http
+        .get(
+          `${process.env.VUE_APP_API_URL}/api/get/users/?userId=${account.userId}`
+        )
+        .then((result) => {
+          console.log(result);
+          if (result.data[0].roles.includes("admin")) {
+            console.log(true);
+            this.isAdmin = true;
+          } else {
+            this.isAdmin = false;
+          }
+        });
+    },
 };
 </script>
